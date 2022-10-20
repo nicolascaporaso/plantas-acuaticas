c44ea61bde363a0754e32367528e49f4ad9cf171
nicolascaporaso <108104290+nicolascaporaso@users.noreply.github.com>
2022-10-20 01:08:47 -0300

    se modifica validacion de form

--- js/contacto/contacto.js
+++ js/contacto/contacto.js
@@ -1,3 +1,33 @@
+let nombre = document.getElementById("nombre");
+let telefono = document.getElementById("telefono");
+let consulta = document.getElementById("consulta");
+let mail = document.getElementById("mail");
+
+nombre.addEventListener('blur', (event) => {
+    nombre.value != "" ? nombre.style.backgroundColor = "green" : nombre.style.backgroundColor = "red";
+    campo = event.target;
+    nombregx = /[^0-9-.%+]{1,12}/;
+    (nombregx.test(campo.value)) ? nombre.style.backgroundColor = "green" : nombre.style.backgroundColor = "red";
+});
+
+mail.addEventListener('blur', (event) => {
+    campo = event.target;
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //condicion ternaria si no es mail valido
+    (emailRegex.test(campo.value)) ? mail.style.backgroundColor = "green" : mail.style.backgroundColor = "red";
+});
+
+telefono.addEventListener('blur', (event) => {
+    campo = event.target;
+    telRegex = /\d\d\d-\d\d-\d\d\d\d-\d\d\d\d\b/;
+    (telRegex.test(campo.value)) ? telefono.style.backgroundColor = "green" : Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: "complete el telefono correctamente xxx-xx-xxxx-xxxx",
+        showConfirmButton: false,
+        timer: 2500
+    });
+});
 
 const btn = document.getElementById('envio_formulario');
 document.getElementById('form')
@@ -31,27 +61,4 @@ document.getElementById('form')
     });
 
 
-let nombre = document.getElementById("nombre");
-let telefono = document.getElementById("telefono");
-let consulta = document.getElementById("consulta");
-let mail = document.getElementById("mail");
 
-nombre.addEventListener('blur', (event) => {
-    nombre.value != "" ? nombre.style.backgroundColor = "green" : nombre.style.backgroundColor = "red";
-    campo = event.target;
-    nombregx = /[^0-9-.%+]{1,12}/;
-    (nombregx.test(campo.value)) ? nombre.style.backgroundColor = "green" : nombre.style.backgroundColor = "red";
-});
-
-mail.addEventListener('blur', (event) => {
-    campo = event.target;
-    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    //condicion ternaria si no es mail valido
-    (emailRegex.test(campo.value)) ? mail.style.backgroundColor = "green" : mail.style.backgroundColor = "red";
-});
-
-telefono.addEventListener('blur', (event) => {
-    campo = event.target;
-    telRegex = /\d\d\d-\d\d-\d\d\d\d-\d\d\d\d\b/;
-    (telRegex.test(campo.value)) ? telefono.style.backgroundColor = "green" : telefono.style.backgroundColor = "red";
-});
