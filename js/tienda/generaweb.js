cee95d5b3b8711b25bf231c16b29f4568db88a62
nicolascaporaso <nicolascaporaso@gmail.com>
2024-03-29 22:14:14 -0300

    mejora plantas

--- js/tienda/generaweb.js
+++ js/tienda/generaweb.js
@@ -32,7 +32,6 @@ const mostrarProductos = (productos) => {
         div.classList.add("tienda__grid__card");
         div.innerHTML += `<h3 class="tienda__grid__card__titulo">Nombre: ${producto.nombre}</h3>
         <p class="tienda__grid__card__parrafo">${producto.descripcion}</p>            
-        <p class="tienda__grid__card__parrafo">Precio: $${producto.precio}</p>
         <div class="position"><p class="tienda__grid__card__parrafo cantidadoculta-carrito posicion" id="compro${producto.id}">cantidad: </p>
         <img class="tienda__grid__card__img" src="${producto.img}" alt="planta acuatica"></div>`
 
